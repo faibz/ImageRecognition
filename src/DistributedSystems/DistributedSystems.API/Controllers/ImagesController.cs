9056c225a055eedd5f3eae88a428f82eeadef02d
faibz <louiswrwright@gmail.com>
2018-10-02 21:32:09 +0100

    started it lol

---
+++ src/DistributedSystems/DistributedSystems.API/Controllers/ImagesController.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using DistributedSystems.API.Models;
+using DistributedSystems.API.Models.Requests;
+using DistributedSystems.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DistributedSystems.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImagesService _imagesService;
+
+        public ImagesController(IImagesService imagesService)
+        {
+            _imagesService = imagesService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubmitImage([FromBody] ImageRequest imageRequest)
+        {
+            var imgStream = new MemoryStream(imageRequest.Image);
+            if (imgStream.Length > 4000000) return BadRequest("Image too large");
+
+            if (!(await _imagesService.UploadImage(new Image(imageRequest), imgStream)).Success) return BadRequest();
+
+            return Ok();
+        }
+    }
+}
