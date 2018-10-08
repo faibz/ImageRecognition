d0234eb7ec138470c7a8e087031599a32d5b0215
faibz <louiswrwright@gmail.com>
2018-10-08 12:50:54 +0100

    added blobcontainer name/reference to config. Removed unused references.

--- src/DistributedSystems/DistributedSystems.API/Startup.cs
+++ src/DistributedSystems/DistributedSystems.API/Startup.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DistributedSystems.API.Factories;
+﻿using DistributedSystems.API.Factories;
 using DistributedSystems.API.Repositories;
 using DistributedSystems.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace DistributedSystems.API
 {
