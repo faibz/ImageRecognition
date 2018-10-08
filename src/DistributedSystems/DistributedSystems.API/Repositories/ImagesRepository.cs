f1ede395d7e0467c602c8137d185077ca96cbeae
faibz <louiswrwright@gmail.com>
2018-10-08 12:37:08 +0100

    Added some more stuff. Who cares about tests? XD

--- src/DistributedSystems/DistributedSystems.API/Repositories/ImagesRepository.cs
+++ src/DistributedSystems/DistributedSystems.API/Repositories/ImagesRepository.cs
@@ -1,5 +1,8 @@
 ﻿using System.Threading.Tasks;
 using DistributedSystems.API.Models;
+using Dapper;
+using DistributedSystems.API.Factories;
+using System.Data;
 
 namespace DistributedSystems.API.Repositories
 {
@@ -10,9 +13,18 @@ namespace DistributedSystems.API.Repositories
 
     public class ImagesRepository : IImagesRepository
     {
-        public Task<Image> InsertImage(Image image)
+        private IDbConnection _connection;
+
+        public ImagesRepository(IDbConnectionFactory connectionFactory)
         {
-            throw new System.NotImplementedException();
+            _connection = connectionFactory.GetDbConnection();
+        }
+
+        public async Task<Image> InsertImage(Image image)
+        {
+            await _connection.ExecuteAsync("meme");
+
+            return new Image(new Models.Requests.ImageRequest());
         }
     }
 }
