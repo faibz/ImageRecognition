31ae4d6424ea53b5b0d9dca2dac90a3cb0c7f269
faibz <louiswrwright@gmail.com>
2018-10-15 17:43:05 +0100

    added migrations project. updated maprepository to reflect new column name.

---
+++ src/DistributedSystems.Migrations/Program.cs
@@ -0,0 +1,30 @@
+﻿using SimpleMigrations;
+using SimpleMigrations.DatabaseProvider;
+using System;
+using System.Data.SqlClient;
+
+namespace DistributedSystems.Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var migrationsAssembly = typeof(Program).Assembly;
+            var connectionString = new SqlConnectionStringBuilder
+            {
+                DataSource = ".",
+                InitialCatalog = "DistributedSystems",
+                IntegratedSecurity = true
+            }.ConnectionString;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var dbProvider = new MssqlDatabaseProvider(connection);
+                var migrator = new SimpleMigrator(migrationsAssembly, dbProvider);
+
+                migrator.Load();
+                migrator.MigrateToLatest();
+            }
+        }
+    }
+}
