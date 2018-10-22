33aa8429d5a42d4f5f113918ab4bf3cc3361663b
Adam Morawiec <amrw@pm.me>
2018-10-22 15:19:47 +0100

    Add Image Analyser
    - take message with the image url off the queue and pass it on to the Azure Computer Vision API,
    - print the API response in console for testing purposes.

---
+++ src/DistributedSystems/DistributedSystems.Worker/ImageAnalyser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using DistributedSystems.API.Models;
+using Newtonsoft.Json;
+
+namespace DistributedSystems.Worker
+{
+    public class ImageAnalyser
+    {
+        private readonly HttpClient _httpClient;
+        private readonly Image _image;
+
+        public ImageAnalyser(string message)
+        {
+            _httpClient = new HttpClient();
+            _image = JsonConvert.DeserializeObject<Image>(message);
+        }
+
+        public async Task<string> SendImageUrlToComputerVisionApiAsync()
+        {
+            var apiRequest = new HttpRequestMessage
+            {
+                RequestUri = new Uri("https://uksouth.api.cognitive.microsoft.com/vision/v1.0/analyze/"),
+                Method = HttpMethod.Post,
+                Content = new StringContent(JsonConvert.SerializeObject(new { url = _image.Location }),
+                                            Encoding.UTF8, "application/json")
+            };
+            apiRequest.Headers.Add("Ocp-Apim-Subscription-Key", "1080c5eef7c04852b1623cc582b0faaf");
+            apiRequest.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var apiResponse = await _httpClient.SendAsync(apiRequest);
+
+            return await apiResponse.Content.ReadAsStringAsync();
+        }
+    }
+}
