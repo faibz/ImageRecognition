c3daef9cf65a10cedd8e3e3ee6a4a59b08201216
Adam Morawiec <42574496+amrwc@users.noreply.github.com>
2018-10-29 18:23:11 +0000

    Add tags forwarding onto the API
    - add Azure Vision response forwarding onto the API,
    - add todo notes.

--- src/DistributedSystems.Worker/QueueListener.cs
+++ src/DistributedSystems.Worker/QueueListener.cs
@@ -1,19 +1,30 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using DistributedSystems.API.Models;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace DistributedSystems.Worker
 {
     public class QueueListener
     {
+        // TODO:
+        // - check response from API before closing the queue,
+        // - check if the confidence is satisfactory,
+
+        private readonly HttpClient _httpClient;
         private readonly IQueueClient _queueClient;
         private ImageAnalyser _imageAnalyser;
 
         public QueueListener(IConfigurationRoot config)
         {
+            _httpClient = new HttpClient();
             _queueClient = new QueueClient(config["ServiceBusConnectionString"], config["ServiceBusQueueName"]);
         }
 
@@ -46,13 +57,38 @@ namespace DistributedSystems.Worker
 
         async Task ProcessMessagesAsync(Message message, CancellationToken token)
         {
-            // Process the message.
-            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+            var messageBodyString = Encoding.UTF8.GetString(message.Body);
+            var bob = JsonConvert.DeserializeObject<Image>(messageBodyString);
 
-            _imageAnalyser = new ImageAnalyser(Encoding.UTF8.GetString(message.Body));
+            // Process the message.
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{messageBodyString}");
+
+            _imageAnalyser = new ImageAnalyser(messageBodyString);
             var analysedPicture = await _imageAnalyser.SendImageUrlToComputerVisionApiAsync();
+            var lx = JObject.Parse(analysedPicture);
+            var bob2 = (JArray)lx["tags"];
+            var tags = bob2.ToObject<Tag[]>();
+
+            var imageTagData = new ImageTagData
+            {
+                ImageId = bob.Id,
+                TagData = tags,
+                Key = bob.ImageKey
+            };
 
             Console.WriteLine(analysedPicture);
+
+            var sendTagsRequest = new HttpRequestMessage
+            {
+                RequestUri = new Uri("https://distsysimageapi.azurewebsites.net/api/Tags/SubmitImageTags"),
+                Method = HttpMethod.Post,
+                Content = new StringContent(JsonConvert.SerializeObject(imageTagData), Encoding.UTF8, "application/json")
+            };
+            sendTagsRequest.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var sendTagsResponse = await _httpClient.SendAsync(sendTagsRequest);
+            Console.WriteLine(sendTagsResponse.StatusCode);
+            Console.WriteLine(sendTagsResponse.ReasonPhrase);
 
             // Complete the message so that it is not received again.
             // This can be done only if the queue Client is created in ReceiveMode.PeekLock mode (which is the default).
