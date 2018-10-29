c537d9270a696ab14a8c0beb5edf5c00af7386c8
U-UNIVERSITY\100374336 <100374336@E03F491C99E1.university.ds.derby.ac.uk>
2018-10-29 16:04:13 +0000

    Started windows service.

---
+++ src/DistributedSystems.WorkerManager/WorkerManagerService.cs
@@ -0,0 +1,37 @@
+﻿using log4net.Core;
+using System;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace DistributedSystems.WorkerManager
+{
+    public partial class WorkerManagerService : ServiceBase
+    {
+        private ILogger _logger;
+        private Timer _timer = new Timer();
+
+        public WorkerManagerService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _logger.Log(typeof(WorkerManagerService), Level.Info, $"Service starting. Time (UTC): {DateTime.UtcNow}", null);
+
+            _timer.Elapsed += new ElapsedEventHandler(DoAThing);
+            _timer.Interval = 5000;
+            _timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            _logger.Log(typeof(WorkerManagerService), Level.Info, $"Service stopping. Time (UTC): {DateTime.UtcNow}", null);
+        }
+
+        private void DoAThing(object sender, ElapsedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
