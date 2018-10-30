d09d3b032d1add2c7a58a0623fa14a1fd43d0e42
faibz <louiswrwright@outlook.com>
2018-10-30 18:05:58 +0000

    Added migrations to rename a column for two reasons: previous name was bad, and because dapper fails to map and it's probably not worth the change. fixed a lot of bugs.

---
+++ src/DistributedSystems.Migrations/Migrations/Up/201810301734_RenameColumnAndRowCountColumns.sql
@@ -0,0 +1,5 @@
+﻿IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Maps') AND COL_LENGTH('Maps', 'ColumnCnt') IS NOT NULL AND COL_LENGTH('Maps', 'RowCnt') IS NOT NULL
+BEGIN
+	EXEC sp_rename 'dbo.Maps.ColumnCnt', 'ColumnCount', 'COLUMN';
+	EXEC sp_rename 'dbo.Maps.RowCnt', 'RowCount', 'COLUMN';
+END
