b5f59b8d2a1d697006c11d8579d86f3336318276
Adam Morawiec <42574496+amrwc@users.noreply.github.com>
2018-11-19 18:51:59 +0000

    Update the ImageStitcher

---
+++ src/DistributedSystems.Worker/KeyedCompoundImage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DistributedSystems.Worker
+{
+    public class KeyedCompoundImage
+    {
+        //TODO: IT'S ONLY HERE TEMPORARILY
+        public IList<MapImageParts> Images {get; set;}
+        public string ImageKey { get; set; }
+    }
+}
