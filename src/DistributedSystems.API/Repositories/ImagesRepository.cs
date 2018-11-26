8da282a7d7e1e1ddf1b706ab8c02b51605460072
U-UNIVERSITY\100374336 <100374336@E03F491C94D1.university.ds.derby.ac.uk>
2018-11-26 12:44:02 +0000

    Modified the migrations. Added endpoints to get average processing times for the worker manager to use.

--- src/DistributedSystems.API/Repositories/ImagesRepository.cs
+++ src/DistributedSystems.API/Repositories/ImagesRepository.cs
@@ -4,6 +4,8 @@ using DistributedSystems.API.Models;
 using Dapper;
 using DistributedSystems.API.Factories;
 using System.Data;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DistributedSystems.API.Repositories
 {
@@ -15,6 +17,7 @@ namespace DistributedSystems.API.Repositories
         Task<string> GetLocationById(Guid imageId);
         Task<Image> GetById(Guid imageId);
         Task UpdateImageProcessedDate(Guid imageId, DateTime dateTime);
+        Task<IList<Image>> GetRecentProcessedImageDates();
     }
 
     public class ImagesRepository : IImagesRepository
@@ -30,7 +33,7 @@ namespace DistributedSystems.API.Repositories
         {
             try
             {
-                await _connection.ExecuteAsync("INSERT INTO [dbo].[Images] ([Id], [Location], [UploadedDate], [ProcessedDate], [Status], [ImageKey]) VALUES (@Id, @Location, @UploadedDate, @Status, @ImageKey)", new { image.Id, image.Location, image.UploadedDate, image.Status, image.ImageKey });
+                await _connection.ExecuteAsync("INSERT INTO [dbo].[Images] ([Id], [Location], [UploadedDate], [Status], [ImageKey]) VALUES (@Id, @Location, @UploadedDate, @Status, @ImageKey)", new { image.Id, image.Location, image.UploadedDate, image.Status, image.ImageKey });
 
                 return true;
             }
@@ -68,9 +71,14 @@ namespace DistributedSystems.API.Repositories
             return (Image) image;
         }
 
-        public async Task UpdateImageProcessedDate(Guid imageId, DateTime dateTime)
+        public async Task UpdateImageProcessedDate(Guid imageId, DateTime dateTime) 
+            => await _connection.ExecuteAsync("UPDATE [dbo].[Images] SET [ProcessedDate] = @ProcessedDate WHERE [Id] = @Id", new { Id = imageId, ProcessedDate = dateTime });
+
+        public async Task<IList<Image>> GetRecentProcessedImageDates()
         {
-            await _connection.ExecuteAsync("UPDATE [dbo].[Images] SET [ProcessedDate] = @ProcessedDate WHERE [Id] = @Id", new { Id = imageId, ProcessedDate = dateTime });
+            var images = await _connection.QueryAsync<Models.DTOs.Image>("SELECT TOP 10 [UploadedDate], [ProcessedDate] FROM [dbo].[Images] ORDER BY [ProcessedDate] DESC");
+
+            return images.Select(img => (Image) img).ToList();
         }
     }
 }
