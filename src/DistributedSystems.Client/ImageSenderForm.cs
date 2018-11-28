357ba055e63ef458a7cf23a3b2fb2c21109c936f
faibz <louiswrwright@gmail.com>
2018-11-28 17:19:51 +0000

    started test client. Note: need to move all models into a class library targetting net standard for them to be accessible from the client project.

---
+++ src/DistributedSystems.Client/ImageSenderForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DistributedSystems.Client
+{
+    public partial class ImageSenderForm : Form
+    {
+        private readonly ImageHelper _imageHelper;
+
+        public ImageSenderForm()
+        {
+            InitializeComponent();
+            _imageHelper = new ImageHelper();
+        }
+
+        private async void SelectImageButton_Click(object sender, EventArgs e)
+        {
+            var dialogResult = FilePickerDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                var file = FilePickerDialog.FileName;
+
+                await SortOutImageStuff(file);
+            }
+
+
+        }
+
+        private async Task SortOutImageStuff(string file)
+        {
+            await _imageHelper.DoImageStuff(file);
+        }
+    }
+}
