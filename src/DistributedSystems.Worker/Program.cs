b4cb848ffc3d19d37fb0bf53fe7a5ce5373ff605
Adam Morawiec <42574496+amrwc@users.noreply.github.com>
2018-12-01 16:38:37 +0000

    Update QueueListeners

--- src/DistributedSystems.Worker/Program.cs
+++ src/DistributedSystems.Worker/Program.cs
@@ -6,8 +6,8 @@ namespace DistributedSystems.Worker
 {
     class Program
     {
-        //public static async Task Main(string[] args)
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
+        //public static void Main(string[] args)
         {
             // Apply the appsettings.json
             var builder = new ConfigurationBuilder()
@@ -15,13 +15,16 @@ namespace DistributedSystems.Worker
                 .AddJsonFile("appsettings.json");
             var configuration = builder.Build();
 
-            var queueListenerSingleImage = new QueueListenerSingleImage(configuration);
-            var queueListenerCompoundImage = new QueueListenerCompoundImage(configuration);
+            //var queueListenerSingleImage = new QueueListenerSingleImage(configuration);
+            //var queueListenerCompoundImage = new QueueListenerCompoundImage(configuration);
 
-            var singleImageListener = queueListenerSingleImage.Run();
-            var compoundImageListener = queueListenerCompoundImage.Run();
-            Task.WaitAll(singleImageListener, compoundImageListener);
+            //var singleImageListener = queueListenerSingleImage.Run();
+            //var compoundImageListener = queueListenerCompoundImage.Run();
+            //Task.WaitAll(singleImageListener, compoundImageListener);
             //Task.WaitAll(singleImageListener, compoundImageListener).Wait(10000);
+
+            var queueListener = new QueueListener(configuration);
+            await queueListener.Run();
         }
     }
 }
@@ -34,16 +37,6 @@ namespace DistributedSystems.Worker
  * - version checker on startup;
  * 
  * 
- * - DONE? do stitching;
- *      - DONE add conditions to stitching to not only stitch to the right;
- * - DONE? send stitched image to the Azure Vision;
- *      - DONE? REWRITE ImageAnalyser FOR CompoundImage's TO USE THE SDK
- *          - https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/quickstarts-sdk/csharp-analyze-sdk
- * - DONE? send the tags to the API;
- * 
- * 
- * - remove TODO from AzureVision: line 59;
- * - remove old Api call from AzureVision class (commented out at the bottom of the file);
  * - remove commented out code snippets from Program.cs;
  * 
  * 
@@ -51,5 +44,7 @@ namespace DistributedSystems.Worker
  * 
  * 
  * - remove TODO from ImageStitcher: line 55;
+ * - remove debug comments from QueueListener;
+ * - delete old QueueListener's;
  * - remove this TODO;
  */
