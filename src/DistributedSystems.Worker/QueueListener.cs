13fc9b7b2cd196a58528e4f06d65b2806621d036
Adam Morawiec <42574496+amrwc@users.noreply.github.com>
2018-12-02 13:20:19 +0000

    Worker works

--- src/DistributedSystems.Worker/QueueListener.cs
+++ src/DistributedSystems.Worker/QueueListener.cs
@@ -4,6 +4,7 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using DistributedSystems.Shared.Models.Requests;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
@@ -13,16 +14,16 @@ namespace DistributedSystems.Worker
     public class QueueListener
     {
         private readonly HttpClient _httpClient;
+        private readonly IConfigurationRoot _config;
         private readonly IQueueClient _queueClient;
         private AzureVision _imageAnalyser;
-        private string _apiSubmitEndpoint;
 
         public QueueListener(IConfigurationRoot config)
         {
             _httpClient = new HttpClient();
-            _queueClient = new QueueClient(config["ServiceBusConnectionString"], config["ServiceBusQueueName"]);
-            _imageAnalyser = new AzureVision(config);
-            _apiSubmitEndpoint = config["ApiSubmitEndpoint"];
+            _config = config;
+            _queueClient = new QueueClient(_config["ServiceBusConnectionString"], _config["ServiceBusQueueName"]);
+            _imageAnalyser = new AzureVision(_config);
         }
 
         public async Task Run()
@@ -30,7 +31,9 @@ namespace DistributedSystems.Worker
             // Register the queue message handler and receive messages in a loop
             RegisterOnMessageHandlerAndReceiveMessages();
             Console.WriteLine("1: finished RegisterOnMessageHandlerAndReceiveMessages");
+
             Console.ReadKey();
+
             await _queueClient.CloseAsync();
         }
 
@@ -50,38 +53,59 @@ namespace DistributedSystems.Worker
 
         async Task ProcessMessagesAsync(Message message, CancellationToken token)
         {
-            Console.WriteLine("5: got into ProcessMessagesAsync");
+            HttpResponseMessage sendTagsResponse;
             var messageBodyString = Encoding.UTF8.GetString(message.Body);
-            string tagData;
+            Console.WriteLine("5: got into ProcessMessagesAsync");
 
             if (message.Label == "single-image")
             {
-                tagData = JsonConvert.SerializeObject(await _imageAnalyser.ProcessSingleImage(messageBodyString));
+                var tagData = await _imageAnalyser.ProcessSingleImage(messageBodyString);
                 Console.WriteLine("3: finished ProcessSingleImage");
+
+                if (tagData.GetType() == typeof(MapTagData))
+                {
+                    var tagDataString = JsonConvert.SerializeObject(tagData);
+                    sendTagsResponse = await SendTags(_config["ApiSubmitMapImagePartTags"], tagDataString);
+                    Console.WriteLine("4: sent stuff to the API");
+                }
+                else
+                {
+                    var tagDataString = JsonConvert.SerializeObject(tagData);
+                    sendTagsResponse = await SendTags(_config["ApiSubmitImageTags"], tagDataString);
+                    Console.WriteLine("4: sent stuff to the API");
+                }
+
+                if (sendTagsResponse.StatusCode == System.Net.HttpStatusCode.OK)
+                    await _queueClient.CompleteAsync(message.SystemProperties.LockToken);
             }
             else if (message.Label == "compound-image")
             {
-                tagData = JsonConvert.SerializeObject(await _imageAnalyser.ProcessCompoundImage(messageBodyString));
+                var tagData = JsonConvert.SerializeObject(await _imageAnalyser.ProcessCompoundImage(messageBodyString));
                 Console.WriteLine("3: finished ProcessCompoundImage");
+
+                sendTagsResponse = await SendTags(_config["ApiSubmitMapCompoundImageTags"], tagData);
+                Console.WriteLine("4: sent stuff to the API");
+
+                if (sendTagsResponse.StatusCode == System.Net.HttpStatusCode.OK)
+                    await _queueClient.CompleteAsync(message.SystemProperties.LockToken);
             }
             else
             {
-                tagData = "";
+                await _queueClient.CompleteAsync(message.SystemProperties.LockToken);
             }
+        }
 
+        private async Task<HttpResponseMessage> SendTags(string endpoint, string tagData)
+        {
             var sendTagsRequest = new HttpRequestMessage
             {
-                RequestUri = new Uri(_apiSubmitEndpoint),
+                RequestUri = new Uri(endpoint),
                 Method = HttpMethod.Post,
                 Content = new StringContent(tagData, Encoding.UTF8, "application/json")
             };
             sendTagsRequest.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var sendTagsResponse = await _httpClient.SendAsync(sendTagsRequest);
-            Console.WriteLine("4: sent stuff to the API");
-
-            if (sendTagsResponse.StatusCode == System.Net.HttpStatusCode.OK)
-                await _queueClient.CompleteAsync(message.SystemProperties.LockToken);
+            return await _httpClient.SendAsync(sendTagsRequest);
         }
 
         // Use this handler to examine the exceptions received on the message pump.
