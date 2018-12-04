23778f49abd0e5cb81b23267e57cc98998f06987
faibz <louiswrwright@outlook.com>
2018-12-04 10:55:19 +0000

    now removes tags that are too long to avoid sql exceptions.

--- src/DistributedSystems.API/Validators/TagsValidator.cs
+++ src/DistributedSystems.API/Validators/TagsValidator.cs
@@ -24,11 +24,27 @@ namespace DistributedSystems.API.Validators
             _compoundImageTagsRepository = compoundImageTagsRepository;
         }
 
-        public async Task ValidateImageTagData(IList<Tag> tags, Guid imageId) 
-            => RemoveMatchingTags(tags, await _tagsRepository.GetTagsByImageId(imageId));
+        public async Task ValidateImageTagData(IList<Tag> tags, Guid imageId)
+        {
+            RemoveLongTags(tags);
+            RemoveMatchingTags(tags, await _tagsRepository.GetTagsByImageId(imageId));
+        }
 
-        public async Task ValidateCompoundImageTagData(IList<Tag> tags, Guid compoundImageId) 
-            => RemoveMatchingTags(tags, (await _compoundImageTagsRepository.GetTagsByCompoundImageId(compoundImageId)).Select(tag => (Tag)tag).ToList());
+        public async Task ValidateCompoundImageTagData(IList<Tag> tags, Guid compoundImageId)
+        {
+            RemoveLongTags(tags);
+            RemoveMatchingTags(tags, (await _compoundImageTagsRepository.GetTagsByCompoundImageId(compoundImageId))
+                .Select(tag => (Tag) tag)
+                .ToList());
+        }
+
+        private static void RemoveLongTags(IList<Tag> tags)
+        {
+            foreach (var tag in tags)
+            {
+                if (tag.Name.Length > 16) tags.Remove(tag);
+            }
+        }
 
         private static void RemoveMatchingTags(ICollection<Tag> primaryList, IEnumerable<Tag> comparisonList)
         {
