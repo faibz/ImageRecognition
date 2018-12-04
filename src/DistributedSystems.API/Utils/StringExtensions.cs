8006824d21536625b47ed45f077b3a06e1b3f758
faibz <louiswrwright@outlook.com>
2018-12-04 00:44:13 +0000

    Added neat string extension to split strings into nice parts. Fixed issue with unauthorized being returned when key is correct enough.

---
+++ src/DistributedSystems.API/Utils/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿//Credit to Martin Liversage from https://stackoverflow.com/questions/4133377/splitting-a-string-number-every-nth-character-number
+
+using System;
+using System.Collections.Generic;
+
+namespace DistributedSystems.API.Utils
+{
+    static class StringExtensions
+    {
+        public static IEnumerable<string> SplitInParts(this string s, int partLength)
+        {
+            if (s == null)
+                throw new ArgumentNullException(nameof(s));
+            if (partLength <= 0)
+                throw new ArgumentException("Part length has to be positive.", nameof(partLength));
+
+            for (var i = 0; i < s.Length; i += partLength)
+                yield return s.Substring(i, Math.Min(partLength, s.Length - i));
+        }
+
+    }
+}
