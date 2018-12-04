a2ebe8ec24543adc22f8f72a8d9bca5c6cc2f154
faibz <louiswrwright@outlook.com>
2018-12-04 13:20:23 +0000

    Maybe I should test these things first.

--- src/DistributedSystems.WorkerManager/WorkerManager/WorkerQueueEvaluator.cs
+++ src/DistributedSystems.WorkerManager/WorkerManager/WorkerQueueEvaluator.cs
@@ -12,6 +12,7 @@ namespace DistributedSystems.WorkerManager.WorkerManager
         internal static async Task<WorkerAction> AdviseAction(int activeWorkerCount, int totalWorkerCount, long queueMessageCount, int targetSla, int minimumWorkerCount, HttpClient httpClient)
         {
             if (activeWorkerCount < minimumWorkerCount) return WorkerAction.Add;
+            if (activeWorkerCount == minimumWorkerCount && queueMessageCount == 0) return WorkerAction.Nothing;
             if (activeWorkerCount > minimumWorkerCount && queueMessageCount == 0) return WorkerAction.Remove;
 
             var result = await (await httpClient.GetAsync("Data/ImageProcessingTime")).Content.ReadAsStringAsync();
